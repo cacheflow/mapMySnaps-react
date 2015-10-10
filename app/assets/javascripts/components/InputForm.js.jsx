bf7922bb57e5aa56778cb983bc809cddd1d72cb8
Lex Alexander <l.alexander10@gmail.com>
2015-10-09 19:02:06 -0700

    Allow user to query and filter retailers -Implemented ability for user to search by city. -Created retailer list component to render retailer data. -Closes #1.

--- app/assets/javascripts/components/InputForm.js.jsx
+++ app/assets/javascripts/components/InputForm.js.jsx
@@ -1,67 +1,67 @@
+var SearchRetailers = React.createClass({
+  getInitialState: function() {
+    return {
+      data: null,
+      city: null
+    };
+  },
 
-
-  var RetailData = React.createClass({
-    getInitialState: function() {
-      return {data: []};
-    },
-
-    loadRetailersFromServer:function() {
-      $.ajax({
-        url: this.props.url,
-        dataType: 'json',
-        cache: false, 
-        type: 'get',
-        success: function(data) {
-          this.setState({data: data})
-        }.bind(this),
-        error: function(xhr, status, error) {
-          console.log(this.props.url, status, error.toString())
-        }.bind(this)
-      })
-    }, 
-
-    componentDidMount: function() {
-      this.loadRetailersFromServer();
-      setInterval(this.loadRetailersFromServer(), this.props.pollInterval)
-    }, 
-
-    render: function() {
+  loadCorrectComponent:function() {
+   if(this.state.city !== null) {
       return (
-        <div> 
-          <RetailDataList data = {this.state.data} />
-        </div>
-      );
-    }
-  });
-
-  var RetailDataList = React.createClass({
-    render: function() {
-      var stores = this.props.data.map(function(store, index) {
-        return (
-          <Retailer retailer={store.retailer} address={store.address} key={index}>
-          </Retailer> 
-        );
-      });
-      return (
-        <div className="retailerList"> 
-          {stores}
-        </div>
+         <RetailerList results={this.state.data} city={this.state.city}/>
       )
-    }
-  });
+   }
+   else {
+     return (
+       <form className="inputForm" ref="inputForm">
+         <input type="text" ref="search" />
+           <input type="submit" value="post" onClick={this.sendForm} />
+        </form>
+     );
+   }
+ },
+  render: function() {
+    return (
+        this.loadCorrectComponent()
+    );
+  },
+  sendForm:function(formData) {
+    formData.preventDefault();
+    var query = React.findDOMNode(this.refs.search).value;
+    $.ajax({
+       url: this.props.url,
+       dataType: 'json',
+       cache: false,
+       type: 'get',
+       success: function(retailerData) {
+         this.setState({
+           data: retailerData,
+           city: query
+         });
+       }.bind(this),
+       error: function(xhr, status, error) {
+         console.log(this.props.url, status, error.toString());
+       }.bind(this)
+     });
+     React.findDOMNode(this.refs.search).value = "";
+  }
+ });
 
-  var Retailer = React.createClass({
-    propTypes: {
-      retailer: React.PropTypes.string,
-      address: React.PropTypes.string
-    }, 
-
-    render: function() {
-      return (
-        <div> 
-        <span> Retailer {this.props.retailer} </span> 
-        <span> Address {this.props.address} </span>
-        </div> 
-      );
-    }
-  });
+var RetailerList = React.createClass({
+   propTypes: {
+     results: React.PropTypes.object,
+     city: React.PropTypes.string
+   },
+   render: function() {
+     var city = this.props.city;
+     var matches = this.props.results.filter(function(retailer, index) {
+       return city === retailer.city.toLowerCase();
+     }).map(function(retailer) {
+       return retailer;
+     });
+     return (
+       <div> {matches}</div>
+     );
+   }
+ });
